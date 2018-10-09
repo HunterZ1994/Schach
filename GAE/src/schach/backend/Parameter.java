e6f189444391f7d19bb41691c0b86e302fceb360
HunterZ1994 <Martin.Thuerauf.1994@gmail.com>
2018-10-09 15:49:29 +0200

    Update Parameter.java

--- GAE/src/schach/backend/Parameter.java
+++ GAE/src/schach/backend/Parameter.java
@@ -1,5 +1,7 @@
 package schach.backend;
-
+/*
+ * Klasse zum Festlegen der maximalen Anzahl an Spielen und dem Parametger der Daten. 
+ */
 public class Parameter {
 	public static final int maxAnzahlSpiele=2048;
 	public static final String pfadKlassenDaten="schach.daten.";
